8dd6ce54efa6cd586b0705efb64d2b8fd1f36d33
shubhamverma377 <shubhamverma377@gmail.com>
2017-07-27 09:37:51 +0530

    ===================

---
+++ app/src/main/java/com/example/android/demoapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("------------------------");
+
+
+        Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
+
+    }
+}
